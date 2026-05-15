d3f5fb6c6c76811d56f12b37650520aaa587a7c7
Hamidulla Nishonboyev <167886343+Hamidulla9@users.noreply.github.com>
2026-05-15 09:24:26 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,26 @@ import os
 import csv
 from datetime import datetime
 
+import pandas as pd
+import pytz
+
 from aiogram import Bot, Dispatcher, F, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from aiogram.fsm.state import StatesGroup, State
+
 from aiogram.types import (
     ReplyKeyboardMarkup,
     KeyboardButton,
     FSInputFile
 )
 
-import pandas as pd
-import pytz
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-# =========================
-# НАСТРОЙКИ
-# =========================
+# =========================================
+# SETTINGS
+# =========================================
 
 TOKEN = "8989441824:AAFieZm6Lpq3q3RG5mlBxEitwitfb7KQ094"
 MY_ID = 8830345316
@@ -57,17 +59,17 @@ PRICES = {
     "Кафельный клей усиленный SOLIDEX 701 - 25k": 35000,
 }
 
-# =========================
+# =========================================
 # BOT
-# =========================
+# =========================================
 
 bot = Bot(token=TOKEN)
 
 dp = Dispatcher(storage=MemoryStorage())
 
-# =========================
-# КЛАВИАТУРЫ
-# =========================
+# =========================================
+# KEYBOARDS
+# =========================================
 
 main_menu = ReplyKeyboardMarkup(
     keyboard=[
@@ -76,9 +78,9 @@ main_menu = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-# =========================
+# =========================================
 # STATES
-# =========================
+# =========================================
 
 class FullOrder(StatesGroup):
     waiting_company = State()
@@ -88,9 +90,9 @@ class FullOrder(StatesGroup):
     waiting_product = State()
     waiting_quantity = State()
 
-# =========================
+# =========================================
 # DATABASE
-# =========================
+# =========================================
 
 def get_next_order_number():
 
@@ -146,9 +148,9 @@ def save_to_db(data):
 
         writer.writerow(data)
 
-# =========================
-# DELETE MESSAGES
-# =========================
+# =========================================
+# DELETE HISTORY
+# =========================================
 
 async def track_msg(message, state):
 
@@ -158,7 +160,9 @@ async def track_msg(message, state):
 
     msg_ids.append(message.message_id)
 
-    await state.update_data(messages_to_delete=msg_ids)
+    await state.update_data(
+        messages_to_delete=msg_ids
+    )
 
 
 async def delete_history(state, chat_id):
@@ -173,47 +177,70 @@ async def delete_history(state, chat_id):
         except:
             pass
 
-    await state.update_data(messages_to_delete=[])
+    await state.update_data(
+        messages_to_delete=[]
+    )
 
-# =========================
+# =========================================
 # START
-# =========================
+# =========================================
 
 @dp.message(Command("start"))
 @dp.message(F.text == "🆕 Новый заказ")
-async def start_order(message: types.Message, state: FSMContext):
+async def start_order(
+    message: types.Message,
+    state: FSMContext
+):
 
     await state.clear()
 
     kb = ReplyKeyboardBuilder()
 
-    kb.add(KeyboardButton(text="🆕 Новый заказ"))
+    kb.add(
+        KeyboardButton(
+            text="🆕 Новый заказ"
+        )
+    )
 
     if message.from_user.id == MY_ID:
-        kb.add(KeyboardButton(text="📊 Выгрузить Excel"))
+
+        kb.add(
+            KeyboardButton(
+                text="📊 Выгрузить Excel"
+            )
+        )
 
     kb.adjust(1)
 
     m = await message.answer(
         "🚀 Введите название организации:",
-        reply_markup=kb.as_markup(resize_keyboard=True)
+        reply_markup=kb.as_markup(
+            resize_keyboard=True
+        )
     )
 
     await track_msg(message, state)
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_company)
+    await state.set_state(
+        FullOrder.waiting_company
+    )
 
-# =========================
+# =========================================
 # COMPANY
-# =========================
+# =========================================
 
 @dp.message(FullOrder.waiting_company)
-async def get_company(message: types.Message, state: FSMContext):
+async def get_company(
+    message: types.Message,
+    state: FSMContext
+):
 
     await track_msg(message, state)
 
-    await state.update_data(company=message.text)
+    await state.update_data(
+        company=message.text
+    )
 
     m = await message.answer(
         "🔢 Введите ИНН (9 цифр):"
@@ -221,18 +248,26 @@ async def get_company(message: types.Message, state: FSMContext):
 
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_inn)
+    await state.set_state(
+        FullOrder.waiting_inn
+    )
 
-# =========================
+# =========================================
 # INN
-# =========================
+# =========================================
 
 @dp.message(FullOrder.waiting_inn)
-async def get_inn(message: types.Message, state: FSMContext):
+async def get_inn(
+    message: types.Message,
+    state: FSMContext
+):
 
     await track_msg(message, state)
 
-    if not message.text.isdigit() or len(message.text) != 9:
+    if (
+        not message.text.isdigit()
+        or len(message.text) != 9
+    ):
 
         m = await message.answer(
             "⚠️ Ошибка! Введите 9 цифр:"
@@ -242,7 +277,9 @@ async def get_inn(message: types.Message, state: FSMContext):
 
         return
 
-    await state.update_data(inn=message.text)
+    await state.update_data(
+        inn=message.text
+    )
 
     m = await message.answer(
         "📸 Пришлите фото паспорта:"
@@ -250,14 +287,22 @@ async def get_inn(message: types.Message, state: FSMContext):
 
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_passport)
+    await state.set_state(
+        FullOrder.waiting_passport
+    )
 
-# =========================
+# =========================================
 # PASSPORT
-# =========================
+# =========================================
 
-@dp.message(FullOrder.waiting_passport, F.photo)
-async def get_passport(message: types.Message, state: FSMContext):
+@dp.message(
+    FullOrder.waiting_passport,
+    F.photo
+)
+async def get_passport(
+    message: types.Message,
+    state: FSMContext
+):
 
     await track_msg(message, state)
 
@@ -284,14 +329,22 @@ async def get_passport(message: types.Message, state: FSMContext):
 
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_geo)
+    await state.set_state(
+        FullOrder.waiting_geo
+    )
 
-# =========================
+# =========================================
 # GEO
-# =========================
+# =========================================
 
-@dp.message(FullOrder.waiting_geo, F.location)
-async def get_geo(message: types.Message, state: FSMContext):
+@dp.message(
+    FullOrder.waiting_geo,
+    F.location
+)
+async def get_geo(
+    message: types.Message,
+    state: FSMContext
+):
 
     await track_msg(message, state)
 
@@ -301,37 +354,51 @@ async def get_geo(message: types.Message, state: FSMContext):
         f"{message.location.longitude}"
     )
 
-    await state.update_data(geo_url=geo_url)
+    await state.update_data(
+        geo_url=geo_url
+    )
 
     builder = ReplyKeyboardBuilder()
 
     for p in PRICES.keys():
-        builder.add(KeyboardButton(text=p))
+
+        builder.add(
+            KeyboardButton(text=p)
+        )
 
     builder.adjust(1)
 
     m = await message.answer(
         "📦 Выберите товар:",
-        reply_markup=builder.as_markup(resize_keyboard=True)
+        reply_markup=builder.as_markup(
+            resize_keyboard=True
+        )
     )
 
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_product)
+    await state.set_state(
+        FullOrder.waiting_product
+    )
 
-# =========================
+# =========================================
 # PRODUCT
-# =========================
+# =========================================
 
 @dp.message(
     FullOrder.waiting_product,
     F.text.in_(PRICES.keys())
 )
-async def get_product(message: types.Message, state: FSMContext):
+async def get_product(
+    message: types.Message,
+    state: FSMContext
+):
 
     await track_msg(message, state)
 
-    await state.update_data(product=message.text)
+    await state.update_data(
+        product=message.text
+    )
 
     m = await message.answer(
         f"Введите количество для {message.text}:",
@@ -340,14 +407,19 @@ async def get_product(message: types.Message, state: FSMContext):
 
     await track_msg(m, state)
 
-    await state.set_state(FullOrder.waiting_quantity)
+    await state.set_state(
+        FullOrder.waiting_quantity
+    )
 
-# =========================
+# =========================================
 # FINISH
-# =========================
+# =========================================
 
 @dp.message(FullOrder.waiting_quantity)
-async def finish_order(message: types.Message, state: FSMContext):
+async def finish_order(
+    message: types.Message,
+    state: FSMContext
+):
 
     if not message.text.isdigit():
         return
@@ -358,11 +430,14 @@ async def finish_order(message: types.Message, state: FSMContext):
 
     quantity = int(message.text)
 
-    total = quantity * PRICES[data["product"]]
-
-    now_str = datetime.now(UZ_TZ).strftime(
-        "%d.%m.%Y %H:%M"
+    total = (
+        quantity *
+        PRICES[data["product"]]
     )
+
+    now_str = datetime.now(
+        UZ_TZ
+    ).strftime("%d.%m.%Y %H:%M")
 
     current_num = get_next_order_number()
 
@@ -382,21 +457,32 @@ async def finish_order(message: types.Message, state: FSMContext):
     save_to_db(order_info)
 
     report = (
-        f"<b>STARTMIX | НОВЫЙ ЗАКАЗ #d0_{current_num}</b>\n"
+        f"<b>STARTMIX | "
+        f"НОВЫЙ ЗАКАЗ "
+        f"#d0_{current_num}</b>\n"
         f"━━━━━━━━━━━━━━━━━━\n"
-        f"👤 <b>Агент:</b> {message.from_user.full_name}\n"
-        f"🏢 <b>Клиент:</b> {data['company']}\n"
-        f"🆔 <b>ИНН:</b> <code>{data['inn']}</code>\n"
-        f"📍 <a href='{data['geo_url']}'>Локация</a>\n"
-        f"🕒 <b>Время:</b> {now_str}\n"
+        f"👤 <b>Агент:</b> "
+        f"{message.from_user.full_name}\n"
+        f"🏢 <b>Клиент:</b> "
+        f"{data['company']}\n"
+        f"🆔 <b>ИНН:</b> "
+        f"<code>{data['inn']}</code>\n"
+        f"📍 <a href='{data['geo_url']}'>"
+        f"Локация</a>\n"
+        f"🕒 <b>Время:</b> "
+        f"{now_str}\n"
         f"━━━━━━━━━━━━━━━━━━\n"
         f"📦 {data['product']}\n"
         f"🔢 {quantity} шт. x "
         f"{PRICES[data['product']]:,} сум\n"
-        f"💰 <b>ИТОГО: {total:,} сум</b>"
+        f"💰 <b>ИТОГО: "
+        f"{total:,} сум</b>"
     ).replace(",", " ")
 
-    await delete_history(state, message.chat.id)
+    await delete_history(
+        state,
+        message.chat.id
+    )
 
     await message.answer_photo(
         photo=data["passport_id"],
@@ -410,7 +496,10 @@ async def finish_order(message: types.Message, state: FSMContext):
         await bot.send_photo(
             chat_id=MY_ID,
             photo=data["passport_id"],
-            caption=f"🚀 КОПИЯ ДЛЯ ОФИСА\n\n{report}",
+            caption=(
+                f"🚀 КОПИЯ ДЛЯ ОФИСА\n\n"
+                f"{report}"
+            ),
             parse_mode="HTML"
         )
 
@@ -419,20 +508,24 @@ async def finish_order(message: types.Message, state: FSMContext):
 
     await state.clear()
 
-# =========================
+# =========================================
 # EXPORT
-# =========================
+# =========================================
 
 @dp.message(Command("export"))
 @dp.message(F.text == "📊 Выгрузить Excel")
-async def export_handler(message: types.Message):
+async def export_handler(
+    message: types.Message
+):
 
     if message.from_user.id != MY_ID:
         return
 
     if not os.path.exists(DB_FILE):
 
-        await message.answer("❌ База пуста.")
+        await message.answer(
+            "❌ База пуста."
+        )
 
         return
 
@@ -440,7 +533,10 @@ async def export_handler(message: types.Message):
 
     excel_file = "report.xlsx"
 
-    df.to_excel(excel_file, index=False)
+    df.to_excel(
+        excel_file,
+        index=False
+    )
 
     await message.answer_document(
         FSInputFile(excel_file),
@@ -449,13 +545,15 @@ async def export_handler(message: types.Message):
 
     os.remove(excel_file)
 
-# =========================
+# =========================================
 # MAIN
-# =========================
+# =========================================
 
 async def main():
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(
+        level=logging.INFO
+    )
 
     await bot.delete_webhook(
         drop_pending_updates=True
@@ -463,9 +561,9 @@ async def main():
 
     await dp.start_polling(bot)
 
-# =========================
+# =========================================
 # RUN
-# =========================
+# =========================================
 
 if __name__ == "__main__":
     asyncio.run(main())
